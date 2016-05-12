b80cd862dfacb0b82e586ef108b45b0952b758bd
Cody Spring <codybamboo@gmail.com>
2016-05-12 12:29:20 -0700

    Added file key for migration goodness.

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  fileKey: process.env.FILE_KEY || '',
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
